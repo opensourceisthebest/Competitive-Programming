d7ff07910ff882344029829225fc8d66b91f8571
Arnav Agarwal <73690811+arnav0929@users.noreply.github.com>
2021-10-01 21:18:40 +0530

    Add files via upload

---
+++ MagicalDoors.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while (t--)
+    {
+        string s;
+        cin>>s;
+
+        int l;
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='0')
+            {
+                l=i;
+                break;
+            }    
+        }
+    
+    int count=1;
+
+    string s1;
+    s1=s.substr(3);
+    
+    if(s1.size()==0)
+        cout<<0<<endl;
+
+    else
+    {
+        for(int i=0;i<s1.size()-1;i++)
+        {
+            if(s[i]!=s[i+1])
+                count++;
+        }
+        cout<<count<<endl;
+    }
+
+    }
+    
+}
